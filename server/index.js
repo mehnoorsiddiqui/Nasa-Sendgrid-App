3852bf5159c31f28f4e5942a2bcf81d8e2cf8a8f
Mehnoor Siddiqui <mehnoorsiddiqui9@gmail.com>
2022-09-12 18:13:19 +0000

    Update 47 files

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const cors = require('cors');
 const api = require('./api')
+const path = require('path');
 
 // Create an express server
 const app = express();
@@ -15,6 +16,14 @@ app.use(express.urlencoded({ extended: true }))
 app.use(cors());
 
 app.use("/api", api);
+
+// Serve any static files
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+// Handle React routing, return all requests to React app
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+});
 
 // Start the server
 app.listen(port, () => console.log(`Server listening at port ${port}`));
